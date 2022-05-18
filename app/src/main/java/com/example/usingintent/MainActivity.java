4337d7ce96c60a1bcec3efaac039dd498878a5d1
Kuramu <81003959+kuramuX@users.noreply.github.com>
2022-05-18 13:09:31 +0800

    UsingIntent

---
+++ app/src/main/java/com/example/usingintent/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.usingintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
